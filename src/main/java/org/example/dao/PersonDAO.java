ae07a8b4a06222352aeda1d85c289606d91b6354
DJ-UKOL <dj-ukol@yandex.ru>
2022-10-12 22:37:28 +0500

    Ex_23  PATCH, DELETE

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -33,4 +33,13 @@ public class PersonDAO {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+
+    public void update(int id, Person updatePerson) {
+        Person personToBeUpdater = show(id);
+        personToBeUpdater.setName(updatePerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
 }
