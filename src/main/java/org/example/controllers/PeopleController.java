647eef16eb6677becd7086e2c974dde3168813a2
DJ-UKOL <dj-ukol@yandex.ru>
2022-10-14 22:43:17 +0500

    Ex_28 JDBC Template

--- src/main/java/org/example/controllers/PeopleController.java
+++ src/main/java/org/example/controllers/PeopleController.java
@@ -56,11 +56,9 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") @Valid Person person,
-                         BindingResult bindingResult,
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
                          @PathVariable("id") int id) {
-
-        if(bindingResult.hasErrors())
+        if (bindingResult.hasErrors())
             return "people/edit";
 
         personDAO.update(id, person);
